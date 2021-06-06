19ef73c9b0a5324bd1fda35639b34a259a8eb1df
lively-form-c463 <dani.mekuriaw@yale.edu>
2021-06-06 02:31:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,7 @@ def key_for_min_value(name_hash)
   min_key = nil
   min_val = 0
   name_hash.each do |key, value|
-    puts min_val
-    puts value
+
     if value < min_val
       min_val = value
       min_key = key
